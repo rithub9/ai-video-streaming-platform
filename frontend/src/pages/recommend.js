8fb1e095b4ffb6dbaad7f3cc7e8802eb88358b7a
ritHUB <Rishithumu27@gmail.com>
2025-02-09 22:43:22 -0500

    Update recommend.js

--- frontend/src/pages/recommend.js
+++ frontend/src/pages/recommend.js
@@ -1,1 +1,20 @@
-recommend
+import React, { useEffect, useState } from "react";
+
+const Recommend = () => {
+  const [recommendation, setRecommendation] = useState(null);
+
+  useEffect(() => {
+    fetch("http://localhost:8003/recommend")
+      .then((res) => res.json())
+      .then((data) => setRecommendation(data));
+  }, []);
+
+  return (
+    <div>
+      <h2>Recommended Video</h2>
+      {recommendation && <h3>{recommendation.recommendation.title}</h3>}
+    </div>
+  );
+};
+
+export default Recommend;
